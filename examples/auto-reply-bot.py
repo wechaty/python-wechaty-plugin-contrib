d21972e8c3b70c39565ac4572be6854e04c39b03
wjmcat <1435130236@qq.com>
2020-12-02 10:43:47 +0800

    fix example lint issue

--- examples/auto-reply-bot.py
+++ examples/auto-reply-bot.py
@@ -1,10 +1,8 @@
 """daily plugin bot examples"""
 import asyncio
-from datetime import datetime
 from typing import List
 
 from wechaty import Wechaty  # type: ignore
-from wechaty_puppet import RoomQueryFilter  # type: ignore
 
 from wechaty_plugin_contrib import (
     AutoReplyRule,
@@ -14,6 +12,7 @@ from wechaty_plugin_contrib import (
 
 
 def get_rules() -> List[AutoReplyRule]:
+    """get the default auto-reply configuration"""
     return [
         AutoReplyRule(keyword='ding', reply_content='dong'),
         AutoReplyRule(keyword='wechaty', reply_content='hello-wechaty')
